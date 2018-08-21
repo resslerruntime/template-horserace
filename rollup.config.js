e64afaf6c530ad3f82402946fa7e6b97b16368ae
Tim Brock <info@datatodisplay.com>
2018-08-21 16:42:19 +0100

    Update modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,16 @@
-var nodeResolve = require("rollup-plugin-node-resolve"),
-    uglify = require("rollup-plugin-uglify");
+var nodeResolve = require("rollup-plugin-node-resolve");
+var uglify = require("rollup-plugin-uglify").uglify;
 
 export default {
-  entry: "src/index.js",
-  format: "iife",
-  moduleName: "template",
-  dest: "template.js",
-  sourceMap: true,
-  plugins: [
-    nodeResolve({ jsnext: true }),
-    uglify(),
-  ]
+	input: "src/index.js",
+	output: {
+		file: "template.js",
+		name: "template",
+		sourcemap: true,
+		format: "iife"
+	},
+	plugins: [
+		nodeResolve({ jsnext: true }),
+		uglify(),
+	]
 };
