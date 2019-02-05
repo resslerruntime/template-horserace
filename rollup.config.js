a6bc1f78d456d7412d4ff414036e574ab3630aa3
Mark Longair <mhl@pobox.com>
2019-02-05 16:11:55 +0000

    Make rollup's errors and warnings more helpful
    This means rollup will error out on actually missing dependencies, and
    ignore the unhelpful "circular dependency" warnings.
    
    See https://github.com/kiln/flourish/issues/1954

--- rollup.config.js
+++ rollup.config.js
@@ -12,5 +12,17 @@ export default {
 	plugins: [
 		nodeResolve({ jsnext: true }),
 		uglify()
-	]
+	],
+	onwarn: function (warning, warn) {
+			if (warning.code === "CIRCULAR_DEPENDENCY") return;
+			if (warning.code === "UNRESOLVED_IMPORT") {
+					throw new Error(
+							"Couldn't resolve the dependency " + warning.source +
+									" (from " + warning.importer + "): sometimes you can" +
+									" fix this with 'npm install', or add '" + warning.source +
+									" to 'external'. See: " + warning.url
+					);
+			}
+			warn(warning);
+	}
 };
